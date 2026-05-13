466fa315bc119b975fa27417f254e8a10dab6b4e
alejandrakn <alejandra.cussianovi@utec.edu.pe>
2026-05-13 16:48:23 -0500

    Delete repository directory

--- repository/BookingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fly_away_travel_api.repository;
-
-import com.example.fly_away_travel_api.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
